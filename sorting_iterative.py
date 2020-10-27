48a0971e6d10d48d5e88619bfd197ef4e7fcd687
Ariane Evans <ariane.evans@student.makeschool.com>
2020-10-26 22:11:04 -0700

    [add] insertion sort code

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -16,8 +16,8 @@ def is_sorted(items):
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n^2) because of nested for loop
+    TODO: Memory usage: O(1) because we are only creating 1 new set variable"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
     
@@ -36,8 +36,9 @@ def bubble_sort(items):
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n^2) but better then bubble sort, because of nested
+    for loop
+    TODO: Memory usage: O(1) because we are only creating a few set variables"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Find minimum item in unsorted items
     # TODO: Swap it with first unsorted item
@@ -59,8 +60,19 @@ def selection_sort(items):
 def insertion_sort(items):
     """Sort given items by taking first unsorted item, inserting it in sorted
     order in front of items, and repeating until all items are in order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n^2) but better then bubble sort and selection sort, 
+    because nested for loop
+    TODO: Memory usage: O(1) because we are only creating small new variables with 
+    set values"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+    indexing_length = range(1, len(items))
+
+    for i in indexing_length:
+        item_to_sort = items[i]
+
+        while items[i - 1] > item_to_sort and i > 0:
+            items[i], items[i - 1] = items[i - 1], items[i]
+            i = i - 1
+    return items
