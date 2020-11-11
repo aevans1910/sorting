c424d5df309fc5c29cabc908bc87143134a79f70
Ariane Evans <ariane.evans@student.makeschool.com>
2020-11-10 19:24:56 -0800

    [add] time and space complexity

--- sorting_recursive.py
+++ sorting_recursive.py
@@ -1,8 +1,10 @@
 def merge(items1, items2):
     """Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n+m) because the while loop has to go through the lengths
+     of both items1 and items2
+    TODO: Memory usage: O(n+m) because we create an array in which we will append 
+    items1 and items2"""
     
     result = []
     left_pointer = right_pointer = 0
@@ -23,8 +25,10 @@ def merge(items1, items2):
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+    TODO: Running time: O(n log n) we will need O(n) time to do the merge 
+    opperation. If that is done properly, we will not need to go through the entire
+    n length of elements in the array again
+    TODO: Memory usage: O(n + m) for the array we create in the merge function"""
 
     if len(items) <= 1:
         return items
@@ -38,17 +42,11 @@ def merge_sort(items):
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
-    that range, moving pivot into index `p`, items less than pivot into range
+    `[low...high]` by choosing a pivot (I chose the item at the end of the array) 
+    from that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Choose a pivot any way and document your method in docstring above
-    # TODO: Loop through all items in range [low...high]
-    # TODO: Move items less than pivot into front of range [low...p-1]
-    # TODO: Move items greater than pivot into back of range [p+1...high]
-    # TODO: Move pivot item into final position [p] and return index p
-
+    TODO: Running time: O(n) because we go through each item in the array once
+    TODO: Memory usage: O(1) because we only create the pivot variable"""
     pivot = items.pop()
 
     for item in items:
@@ -65,13 +63,11 @@ def partition(items, low, high):
 def quick_sort(items):
     """Sort given items in place by partitioning items in range `[low...high]`
     around a pivot item and recursively sorting each remaining sublist range.
-    TODO: Best case running time: ??? Why and under what conditions?
-    TODO: Worst case running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check if high and low range bounds have default values (not given)
-    # TODO: Check if list or range is so small it's already sorted (base case)
-    # TODO: Partition items in-place around a pivot and get index of pivot
-    # TODO: Sort each sublist range by recursively calling quick sort
+    TODO: Best case running time: O(n log n) because in the best case situation we
+    woudln't need to go through every element again if it is partitioned properly
+    TODO: Worst case running time: O(n^2) if we don't partition well and therefore
+    have to go through each element twice
+    TODO: Memory usage: O(2n) because we create the low and high arrays"""
     low = []
     high = []
 
@@ -81,12 +77,3 @@ def quick_sort(items):
         partition(items, low, high)
 
     return quick_sort(items)
-
-    # low = partition(low)
-    # partition(high)
-
-    # for item in items:
-    #     if item > pivot():
-    #         high.append(item)
-    #     else:
-    #         low.append(item)
