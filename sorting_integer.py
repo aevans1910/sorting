2b512b6d90a1f263682aff8ae3e14a7313556a62
Ariane Evans <ariane.evans@student.makeschool.com>
2020-11-15 19:32:34 -0800

    [add] counting sort

--- sorting_integer.py
+++ sorting_integer.py
@@ -8,6 +8,21 @@ def counting_sort(numbers):
     # TODO: Loop over given numbers and increment each number's count
     # TODO: Loop over counts and append that many numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+    count_arr = []
+    sorted_arr = []
+    min_num = numbers[0]
+    max_num = numbers[0]
+    for num in numbers:
+        count_arr[num] += 1
+        if num < min_num:
+            min_num = num
+        elif num > max_num:
+            max_num = num
+    for i in range(min_num, max_num):
+        while count_arr[i] > 0:
+            sorted_arr.append(i)
+            count_arr[i] -= 1
+
 
 
 def bucket_sort(numbers, num_buckets=10):
